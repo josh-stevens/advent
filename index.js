aaec106932c4a6d456598ba8586b8d297794fa46
Josh Stevens <jcstevens12@outlook.com>
2016-12-01 11:11:25 -0600

    Solve day one

--- index.js
+++ index.js
@@ -17,6 +17,90 @@ var dayOneInput = ['L2', 'L5', 'L5', 'R5', 'L2', 'L4', 'R1', 'R1', 'L4', 'R2', '
                                  'L2', 'L1', 'R4']
 
 var dayOne = exports.dayOne = function(input) {
+  var directions = ['N', 'E', 'S', 'W'];
+  var currentDir = directions[0];
+  var currentPos = [0, 0];
+  var cache = [];
+  var solution;
+
+  var changeDirection = function(turn) {
+    var currentIdx = directions.indexOf(currentDir);
+
+    if (turn === 'R') {
+      currentDir = directions[currentIdx + 1];
+      if (currentDir === undefined) {
+        currentDir = directions[0];
+      }
+    } else {
+      currentDir = directions[currentIdx - 1];
+      if (currentDir === undefined) {
+        currentDir = directions[3];
+      }
+    }
+  };
+
+  var checkPos = function() {
+    console.log("checking cache for position ", currentPos);
+    var seen = false;
+    cache.forEach(function(previous) {
+      if (previous[0] === currentPos[0] && previous[1] === currentPos[1]) {
+        seen = true;
+      }
+    });
+
+    if (seen) {
+      console.log("already been here")
+      solution = currentPos;
+      Object.freeze(solution);
+    } else {
+      console.log("new location ")
+      cache.push(currentPos.slice(0));
+    }
+  };
+
+  var move = function(distance) {
+    switch (currentDir) {
+      case 'N':
+        while(distance) {
+          currentPos[1]++;
+          distance--;
+          checkPos();
+        }
+        break;
+      case 'S':
+        while(distance) {
+          currentPos[1]--;
+          distance--;
+          checkPos();
+        }
+        break;
+      case 'E':
+        while(distance) {
+          currentPos[0]++;
+          distance--;
+          checkPos();
+        }
+        break;
+      case 'W':
+        while(distance) {
+          currentPos[0]--;
+          distance--;
+          checkPos();
+        }
+        break;
+    }
+  };
+
+  input.forEach(function(instruction) {
+    var turn = instruction.slice(0, 1);
+    var distance = instruction.slice(1);
+    changeDirection(turn);
+    move(parseInt(distance));
+  });
+
+  return solution.reduce(function(total, val) {
+    return total + Math.abs(val);
+  }, 0);
 };
 
 var dayOneResult = dayOne(dayOneInput);
--- test/dayOne.js
+++ test/dayOne.js
@@ -3,5 +3,29 @@ var solver = require('../index.js');
 
 describe('Day One', function() {
 
+  // task one has been ignored to complete task 2
+  xit('should resolve the first example', function() {
+    var exampleOne = ['R2', 'L3'];
+    var result = solver.dayOne(exampleOne);
+    assert.equal(5, result);
+  });
+
+  xit('should resolve the second example', function() {
+    var exampleTwo = ['R2', 'R2', 'R2'];
+    var result = solver.dayOne(exampleTwo);
+    assert.equal(2, result);
+  });
+
+  xit('should resolve the third example', function() {
+    var exampleThree = ['R5', 'L5', 'R5', 'R3'];
+    var result = solver.dayOne(exampleThree);
+    assert.equal(12, result);
+  });
+
+  it('should resolve the example for task two', function() {
+    var taskTwoExample = ['R8', 'R4', 'R4', 'R8'];
+    var result = solver.dayOne(taskTwoExample);
+    assert.equal(4, result);
+  });
 
 });
